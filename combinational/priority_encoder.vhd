ff3202280cb96717802c337faeda46b9d47a6ac7
Greg Stitt <gstitt@ece.ufl.edu>
2022-08-18 19:43:51 +0000

    Fixed typo

--- combinational/priority_encoder.vhd
+++ combinational/priority_encoder.vhd
@@ -6,7 +6,7 @@
 -- inputs. The module assumes that the MSB of the input is highest priority.
 
 -- NOTE: These examples might not synthesize efficiently in all tools.
--- See https://opencores.org/projects/priority_encoder for alternative.
+-- See https://opencores.org/projects/priority_encoder for an alternative.
 
 
 library ieee;
